8aa45c3f0a04b0941ade17e60e26e8b1d4c92230
Hojjat Jokar <paintthedream@gmail.com>
2020-03-26 17:48:11 +0330

    feat(DoublyLinkedList): add push method

---
+++ src/DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    if (!value) return;
+
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.length++;
+    return this;
+  }
+}
