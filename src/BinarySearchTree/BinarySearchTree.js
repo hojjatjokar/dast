a0e08b1c978f48a63cca93a2e90d84272292243c
Hojjat Jokar <paintthedream@gmail.com>
2020-04-09 17:44:44 +0330

    feat(BST): add BST class

---
+++ src/BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+  }
+}
