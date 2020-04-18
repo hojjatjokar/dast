850849c65bb6aa751dfd7e1123281fef3a5c6233
Hojjat Jokar <paintthedream@gmail.com>
2020-04-18 23:28:13 +0330

    feat(PriorityQueue): add priority queue

---
+++ src/PriorityQueue/PriorityQueue.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(value, priority) {
+    const node = new Node(value, priority);
+    this.values.push(node);
+    this.bubbleUp();
+  }
+
+  dequeue() {
+    const result = this.values.shift();
+    this.sinkIn();
+    return result;
+  }
+}
