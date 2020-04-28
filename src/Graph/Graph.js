a175b124c916946461215ecefc245453d77f19bc
Hojjat Jokar <paintthedream@gmail.com>
2020-04-28 08:13:59 +0330

    feat(Graph): add edge method

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -4,6 +4,22 @@ class Graph {
   }
 
   addVertex(vertex) {
-    if (this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
+
+const routes = new Graph();
+
+routes.addVertex('London');
+routes.addVertex('Rome');
+routes.addVertex('Milan');
+routes.addVertex('Berlin');
+
+routes.addEdge('London', 'Rome');
+
+console.log(routes);
