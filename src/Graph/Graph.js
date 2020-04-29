f0c796dd5f5d62a71e5ce8987c48d0c11341aa15
Hojjat Jokar <paintthedream@gmail.com>
2020-04-29 21:12:09 +0330

    feat(Graph): add remove method

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -11,6 +11,15 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (item) => item !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (item) => item !== vertex1
+    );
+  }
 }
 
 const routes = new Graph();
@@ -21,5 +30,9 @@ routes.addVertex('Milan');
 routes.addVertex('Berlin');
 
 routes.addEdge('London', 'Rome');
+
+console.log(routes);
+
+routes.removeEdge('London', 'Rome');
 
 console.log(routes);
